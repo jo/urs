84578819f53a9eaca0163741b2ba3b225fea0ff4
Johannes J. Schmidt <schmidt@netzmerk.com>
2013-04-11 12:46:43 +0200

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,99 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+
+    // Task configuration.
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      js: {
+        src: ['lib/<%= pkg.name %>.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      },
+      css: {
+        src: ['lib/<%= pkg.name %>.css'],
+        dest: 'dist/<%= pkg.name %>.css'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.js.dest %>',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    cssmin: {
+      dist: {
+        src: '<%= concat.css.dest %>',
+        dest: 'dist/<%= pkg.name %>.min.css'
+      }
+    },
+    nodeunit: {
+      files: ['test/**/*_test.js']
+    },
+    jshint: {
+      options: {
+        jshintrc: '.jshintrc'
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib: {
+        options: {
+          jshintrc: 'lib/.jshintrc'
+        },
+        src: ['lib/**/*.js']
+      },
+      test: {
+        src: ['test/**/*.js']
+      }
+    },
+    csslint: {
+      options: {
+        'unqualified-attributes': false
+      },
+      src: ['lib/**/*.css']
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib: {
+        files: '<%= jshint.lib.src %>',
+        tasks: ['jshint:lib', 'nodeunit']
+      },
+      test: {
+        files: '<%= jshint.test.src %>',
+        tasks: ['jshint:test', 'nodeunit']
+      }
+    },
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'csslint', 'nodeunit', 'concat', 'uglify', 'cssmin']);
+
+};
---
+++ README.md
@@ -0,0 +1,8 @@
+# Urs
+Ultra Responsive Slider
+
+[Example](http://jo.github.io/urs)
+
+## License
+Copyright (c) 2013 Johannes J. Schmidt  
+Licensed under the MIT license.
